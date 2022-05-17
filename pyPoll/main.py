8d3b2ca6ee681e1b64386585b8f5d9c77817d765
Naomi Gebru <95658342+naomigebru86@users.noreply.github.com>
2022-05-17 16:23:10 -0400

    Update main.py

--- pyPoll/main.py
+++ pyPoll/main.py
@@ -1,29 +1,66 @@
+# import the csv file
 import csv 
 file = open("pyPoll/election_data.csv")
+
+# register the csv file to csv_file variable
 csv_file=csv.reader(file)
 header=[]
 rows=[]
+
+# create a nested list of rows 
 for row in csv_file:
     rows.append(row)
 
+# get total votes
+total = len(rows)-1
+
+#append candiate names to a list 
 candidate_name =[]
-for i in rows:
-    candidate_name.append(i[2])
-dict_n={"Khan":0,"Correy":0,"Li":0, "O'Tooley":0}
-for i in range(1,len(candidate_name)): 
-    dict_n[candidate_name[i]]+=1
-Khan_average=(dict_n["Khan"]/(len(rows)-1))*100
-Li_average=(dict_n["Li"]/(len(rows)-1))*100
-Correy_average=(dict_n["Correy"]/(len(rows)-1))*100
+for i in range(0,len(rows)):
+    candidate_name.append(rows[i][2])
+
+# create a dictionary of candidates
+dict_n={}
+for i in range(1,len(candidate_name)):
+    if candidate_name[i] in dict_n.keys():
+        dict_n[candidate_name[i]]+=1
+    else:
+         dict_n[candidate_name[i]]=1
+print(dict_n)
+
+# dictionary of candidates with thier number of votes and percente
+dict_candidates={}
+for k,v in dict_n.items():
+    dict_candidates[k]=[round((v/total)*100,2),v]
+print(dict_candidates)
+
+#get the winner
+winner =""
+max= 0
+for k,v in dict_candidates.items():
+    if v[1]>max:
+        max=v[1]
+        winner=k
+
+#print the output to console
 print(f"Election Votes")
 print(f"--------------------------")
-print(f"total votes={len(rows)-1}")
+print(f"total votes={total}")
 print(f"---------------------------")
-print(dict_n)
-print(f"Khan average= {Khan_average}")
-print(f"Li average= {Li_average}")
-print(f"Correy average= {Correy_average}")
+for k,v in dict_candidates.items():
+    print(f"{k}: {v[0]}% ({v[1]})")
 print(f"---------------------------")
-print(f"Winner: Khan")
+print(f"Winner: {winner}")
 print(f"---------------------------")
 
+# save the output to text file
+text_election_analysis = open("analysis/Election Results","a")
+text_election_analysis.write("Election Results")
+text_election_analysis.write(f"\n-------------------------")
+text_election_analysis.write(f"\nTotal Votes: {total}")
+text_election_analysis.write(f"\n-------------------------")
+for k,v in dict_candidates.items():
+    text_election_analysis.write(f"\n{k}: {v[0]}% ({v[1]})")
+text_election_analysis.write(f"\n-------------------------")
+text_election_analysis.write(f"\nWinner: {winner}")
+text_election_analysis.write(f"\n-------------------------")
